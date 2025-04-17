e7cc01a5ab841d63a51718dc7346c88e030252bb
MiYaS1 <miyashirodietaryedu@gmail.com>
2025-04-17 21:54:51 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,48 +1,26 @@
-const CACHE_NAME = "memory-game-cache-v1";
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./Build/Dietary-Education-Memory-Game.loader.js",
-  "./Build/Dietary-Education-Memory-Game.framework.js",
-  "./Build/Dietary-Education-Memory-Game.data",
-  "./Build/Dietary-Education-Memory-Game.wasm",
-  "./TemplateData/style.css",
-  "./image/icon-192x192.png",
-  "./image/icon-512x512.png"
-];
-
-// インストール
-self.addEventListener("install", event => {
+self.addEventListener('install', function (event) {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open('unity-cache-v1').then(function (cache) {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './TemplateData/style.css',
+        './Build/Unity-Dietary-Education-Memory-Game.loader.js',
+        './Build/Unity-Dietary-Education-Memory-Game.framework.js',
+        './Build/Unity-Dietary-Education-Memory-Game.data',
+        './Build/Unity-Dietary-Education-Memory-Game.wasm',
+        './image/icon-192x192.png',
+        './image/icon-512x512.png'
+      ]);
+    })
   );
 });
 
-// リクエスト時
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', function (event) {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
-
-// 古いキャッシュ削除
-self.addEventListener("activate", event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys()
-      .then(keys =>
-        Promise.all(
-          keys.map(key => {
-            if (!cacheWhitelist.includes(key)) {
-              return caches.delete(key);
-            }
-          })
-        )
-      )
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
   );
 });
