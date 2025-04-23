dcee04359a3a79e25e45a3104903e02dd0f7edaa
MiYaS1 <miyashirodietaryedu@gmail.com>
2025-04-23 22:31:26 +0900

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-// サービスワーカーを登録しないコード
-if ('serviceWorker' in navigator) {
-  // service-worker.jsは使わない
-  console.log('Service Worker is disabled.');
-} else {
-  console.log('Service Worker is not supported by the browser.');
-}
